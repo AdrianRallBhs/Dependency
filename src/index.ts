c7c09d722a220423eebdb5221b8ee82577fe8111
Adrian Rall <adrian@asda4.de>
2022-11-22 18:17:52 +0100

    getRepoUrl Test #1

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,23 @@
 import * as Core from "@actions/core"
+import * as Github from "@actions/github"
+import { context } from "@actions/github/lib/utils"
+
+type GithubContext = typeof Github.context
 
 const inputName = Core.getInput("name")
 
-greet(inputName)
+greet(inputName, getRepoUrl(context))
 
-function greet(name: string) {
-    console.log(`'Hello ${name}!'`)
+
+
+
+function greet(name: string, repoUrl: string) {
+    console.log(`'Hello ${name}! You r running a GH action in ${repoUrl}'`)
 }
-
+//ctx = context
+//repo is repository
+//if Enterprise-Server is used there u need serverUrl, otherwise it is the same
+function getRepoUrl({repo, serverUrl}: GithubContext): string  {
+   return `${serverUrl}/${repo.owner}/${repo.repo}`
+}
 console.log('Hello World!')
