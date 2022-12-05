1ee5b5b5cae3eb888acf8d505e76bf505eb8f2f0
Adrian Rall <adrian@asda4.de>
2022-12-05 22:55:24 +0100

    nochmaliger weiterer revert

--- src/index.ts
+++ src/index.ts
@@ -8,10 +8,5 @@ const inputName = Core.getInput("name")
 function greet(name: string, repoUrl: string) {
     console.log(`'Hello ${name}! You r running a GH action in ${repoUrl}'`)
 }
-//ctx = context
-//repo is repository
-//if Enterprise-Server is used there u need serverUrl, otherwise it is the same
-function getRepoUrl({ repo, serverUrl }: GithubContext): string {
-    return `${serverUrl}/${repo.owner}/${repo.repo}`
-}
+
 console.log('Hello World!')
